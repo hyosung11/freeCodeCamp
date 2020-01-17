09c5ea897ee8152c369c67cebe27007dff30b768
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-17 17:24:28 -0500

    through 29

--- README.md
+++ README.md
@@ -122,3 +122,9 @@ this.setState((state, props) => ({
   counter: state.counter + props.increment
 }));
 ```
+- Create a Controlled Input
+  - Your application may have more complex interactions between state and the rendered UI.
+  - For example, form control elements for text input, such as `input` and `textarea`, maintain their own state in the DOM as the user types.
+  - With React, you can move this mutable state into a React component's `state`.
+  - The user's input becomes part of the application `state`, so React controls the value of that input field.
+  - Typically, if you have React components with input fields the user can type into, it will be a controlled input form.
--- react.jsx
+++ react.jsx
@@ -594,3 +594,79 @@ class Counter extends React.Component {
     );
   }
 };
+
+
+{/* 28. Create a Controlled Input */}
+
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // change code below this line
+    this.handleChange = this.handleChange.bind(this);
+    // change code above this line
+  }
+  // change code below this line
+  handleChange(event) {
+    this.setState(state => ({
+      input: event.target.value
+    }))
+  }
+
+  // change code above this line
+  render() {
+    return (
+      <div>
+        { /* change code below this line */}
+        <input value={this.state.input} onChange={this.handleChange}/>
+        { /* change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
+
+
+{/* 29. Create a Controlled Form */}
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      submit: this.state.input
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.input}
+            onChange={this.handleChange}/>
+          <button type='submit'>Submit!</button>
+        </form>
+          <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+};
